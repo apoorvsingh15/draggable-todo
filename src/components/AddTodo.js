12a976250d03f1c4ec7ebb305548d6828f0d56a4
Apoorv Singh <apoorv.singh15@gmail.com>
2019-10-11 20:13:40 +0530

    added style changes

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -9,9 +9,6 @@ const AddTodo = (props) => {
   const [listItems, setListItems] = useState([]);
 
   const ref = useRef(null);
- 
-    // to determine the mode of todo list - edit and normal
-  // enables user to edit todo going in edit mode
 
   return(
     <Card style={{ width: '20rem', paddingBottom: '80px' }}>
@@ -34,13 +31,13 @@ const AddTodo = (props) => {
           {provided.placeholder}
             {props.listItems.map((listItem, key) => 
               <List 
-              key={key} 
-              index={key}
-              listItem={listItem} 
-              setListItems={props.setListItems}
-              listItems={props.listItems}
-              onSaveClick={props.onSaveClick}
-              onPressEnter={props.onPressEnter}
+                key={key} 
+                index={key}
+                listItem={listItem} 
+                setListItems={props.setListItems}
+                listItems={props.listItems}
+                onSaveClick={props.onSaveClick}
+                onPressEnter={props.onPressEnter}
               />
             )} 
           </div>
